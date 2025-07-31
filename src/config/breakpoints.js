01a64b0e2bf9ca3082e1a67c6a38ecbec668278d
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2025-07-31 16:51:49 +0500

    chores

--- src/config/breakpoints.js
+++ src/config/breakpoints.js
@@ -3,6 +3,6 @@ export const BREAKPOINTS = {
   XS: 0, // Mobile
   SM: 576, // Small
   MD: 768, // Tablet
-  LG: 992, // Desktop
-  XL: 1200, // Large desktop (optional)
+  LG: 1100, // Desktop
+  XL: 1280, // Large desktop (optional)
 };
